73e8e369e64c74dd5018eb8eade79d7b1c66b5a9
ramu1702 <ramudusiram28@gmail.com>
2026-07-09 17:29:22 +0530

    modify

--- js/loader.js
+++ js/loader.js
@@ -1,79 +1,11 @@
-const loader = document.getElementById("loader");
+window.addEventListener("load", () => {
 
-const loaderName = document.getElementById("loader-name");
+    const loader = document.getElementById("loader");
 
-const role = document.getElementById("loader-role");
-
-const logo = document.getElementById("loader-logo");
-
-const tagline = document.getElementById("loader-tagline");
-
-const fullName = "SITA RAMUDU";
-
-let i = 0;
-
-// Typing animation
-function typeName(){
-
-    if(i < fullName.length){
-
-        loaderName.textContent += fullName.charAt(i);
-
-        i++;
-
-        setTimeout(typeName,120);
-
+    if(loader){
+        loader.style.display = "none";
     }
 
-    else{
+    document.body.style.overflow = "auto";
 
-        showRole();
-
-    }
-
-}
-
-function showRole(){
-
-    role.style.opacity="1";
-
-    setTimeout(showLogo,800);
-
-}
-
-function showLogo(){
-
-    loaderName.style.transition=".8s";
-    role.style.transition=".8s";
-
-    loaderName.style.opacity="0";
-    role.style.opacity="0";
-
-    setTimeout(()=>{
-
-        loaderName.style.display="none";
-        role.style.display="none";
-
-        logo.classList.add("logo-show");
-
-        tagline.style.opacity="1";
-
-    },700);
-
-    setTimeout(openPortfolio,1800);
-
-}
-
-function openPortfolio(){
-
-    loader.classList.add("fade-out");
-
-    setTimeout(()=>{
-
-        loader.style.display="none";
-
-    },800);
-
-}
-
-typeName();
+});
